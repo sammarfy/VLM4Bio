9b5be7809e2b5b25964a0add70bbbfbd788d8f56
M. Maruf <51701844+sammarfy@users.noreply.github.com>
2024-06-14 07:04:29 -0400

    prompting update

--- zero_shot_eval/classification_prompting.py
+++ zero_shot_eval/classification_prompting.py
@@ -1,20 +1,18 @@
-##################################################################################################################################################
-# class ----> GPT_4V         model_name ----> "gpt-4v"                                 (implemented, env: llava)                                 #
-# class ----> LLaVA          model_name ----> "llava-v1.5-7b"                          (implemented, env: llava)                                 #
-#                                       ----> "llava-v1.5-13b"                         (implemented, env: llava)                                 #
-# class ----> OFA            model_name ----> "ofa-large"                                                                                        #
-#                                       ----> "ofa-huge"                                                                                         #
-# class ----> CogVLM         model_name ----> "cogvlm-grounding-generalist"            (implemented, env: vlm_env, gpu:2, --ntasks-per-node=8)   #
-#                                       ----> "cogvlm-chat"                            (implemented, env: vlm_env, gpu:2, --ntasks-per-node=8)   #
-# class ----> MinGPT4        model_name ----> "minigpt4-vicuna-7B"                     (implemented, env: minigptv)                              #
-#                                       ----> "minigpt4-vicuna-13B"                    (implemented, env: minigptv)                              #
-# class ----> BLIP-2FLAN     model_name ----> "blip-flan-xxl"                          (implemented, env: blip, --cpus-per-task=8)               #
-#                                       ----> "blip-flan-xl"                           (implemented, env: blip, --cpus-per-task=8)               #
-# class ----> Instruct_BLIP  model_name ----> "instruct-vicuna7b"                      (implemented, env: instruct_blip, --cpus-per-task=8)      #
-#                                       ----> "instruct-vicuna13b"                     (implemented, env: instruct_blip, --cpus-per-task=8)      #
-#                                       ----> "instruct-flant5xl"                      (implemented, env: instruct_blip, --cpus-per-task=8)      #
-#                                       ----> "instruct-flant5xxl"                     (implemented, env: instruct_blip, --cpus-per-task=8)      #
-##################################################################################################################################################
+##############################################################################
+# class ----> GPT_4V         model_name ----> "gpt-4v"                       #
+# class ----> LLaVA          model_name ----> "llava-v1.5-7b"                #
+#                                       ----> "llava-v1.5-13b"               #
+# class ----> CogVLM         model_name ----> "cogvlm-chat"                  #
+# class ----> MinGPT4        model_name ----> "minigpt4-vicuna-7B"           #
+#                                       ----> "minigpt4-vicuna-13B"          #
+# class ----> BLIP-2FLAN     model_name ----> "blip-flan-xxl"                #
+#                                       ----> "blip-flan-xl"                 #
+# class ----> Instruct_BLIP  model_name ----> "instruct-vicuna7b"            #
+#                                       ----> "instruct-vicuna13b"           #
+#                                       ----> "instruct-flant5xl"            #
+#                                       ----> "instruct-flant5xxl"           #
+##############################################################################
+
 
 import json
 from tqdm import tqdm
@@ -22,64 +20,47 @@ import argparse
 import os
 import warnings
 warnings.filterwarnings('ignore')
-
-##################################################################################################################################################
+import os.path as osp
 import sys
 current_dir = os.path.dirname(__file__)
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(1, parent_dir)
-##################################################################################################################################################
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--model", "-m", type=str, default='llava-v1.5-7b', help="multimodal-model, option: 'gpt-4v', 'llava-v1.5-7b', 'llava-v1.5-13b', 'cogvlm-grounding-generalist', 'cogvlm-chat'")
+parser.add_argument("--model", "-m", type=str, default='llava-v1.5-7b', help="multimodal-model, option: 'gpt-4v', 'gpt-4o','llava-v1.5-7b', 'llava-v1.5-13b', 'cogvlm-chat', 'minigpt4-vicuna-7B', 'minigpt4-vicuna-13B', 'blip-flan-xl', 'blip-flan-xxl', 'instruct-vicuna7b', 'instruct-vicuna13b', 'instruct-flant5xl', 'instruct-flant5xxl'")
 parser.add_argument("--task_option", "-t", type=str, default='direct', help="task option: 'direct', 'selection' ")
+parser.add_argument("--result_dir", "-o", type=str, default='results/', help="path to output")
+parser.add_argument("--data_dir", "-p", type=str, default='data/', help="path to datasets")
 parser.add_argument("--num_queries", "-n", type=int, default=-1, help="number of images to query from dataset")
 parser.add_argument("--chunk_id", "-c", type=int, default=0, help="0, 1, 2, 3, 4, 5, 6, 7, 8, 9")
 parser.add_argument("--technique", "-q", type=str, default="contextual", help="technique option: 'contextual', 'dense-caption', 'cot', 'no-prompting'")
-
-# updated
 parser.add_argument("--dataset", "-d", type=str, default='fish-prompting', help="dataset option: 'fish-prompting','bird-prompting', 'butterfly-prompting' ")
-parser.add_argument("--server", "-s", type=str, default='arc', help="server option: 'osc', 'pda'")
+parser.add_argument("--llava_model_dir", "-l", type=str, default='/projects/ml4science/maruf/new_llava_models/') # We saved the LLaVA-v1.5-7b and LLaVA-v1.5-13b models in this path.
+
 args = parser.parse_args()
 
-if args.server == "arc":
-    root_dir = '/projects/ml4science/'
-elif args.server == "osc":
-    root_dir = '/fs/ess/PAS2136/'
-elif args.server == "pda":
-    root_dir = '/data/'
-
-
-# if args.dataset == 'fish-10k':
-
-#     args.result_dir = root_dir+'VLM4Bio/results/fish-10k'
-    
-#     images_list_path = root_dir+'VLM4Bio/datasets/Fish/metadata/imagelist_10k.txt'
-#     image_dir = root_dir+'VLM4Bio/datasets/Fish/images'
-#     img_metadata_path = root_dir+'VLM4Bio/datasets/Fish/metadata/metadata_10k.csv'
-#     organism = 'fish'
 
 if args.dataset == 'fish-prompting':
-    args.result_dir = root_dir+'VLM4Bio/prompting_results/fish-prompting'
-    images_list_path = root_dir+'VLM4Bio/datasets/Fish/metadata/imagelist_prompting.txt'
-    image_dir = root_dir+'VLM4Bio/datasets/Fish/images'
-    img_metadata_path = root_dir+'VLM4Bio/datasets/Fish/metadata/metadata_prompting.csv'
+    args.result_dir = osp.join(args.result_dir, 'fish-prompting')
+    images_list_path = osp.join(args.data_dir, 'VLM4Bio/datasets/Fish/metadata/imagelist_prompting.txt')
+    image_dir = osp.join(args.data_dir, 'VLM4Bio/datasets/Fish/images')
+    img_metadata_path = osp.join(args.data_dir, 'VLM4Bio/datasets/Fish/metadata/metadata_prompting.csv')
     organism = 'fish'
     dense_caption_instruction = "Generate a detailed dense caption for the image of the fish, focusing on describing all visible anatomical details such as the shape and size of the fins, presence and absence of fins, color patterns, body shape, and any distinctive markings. Mention any specific features that are typically used in the classification of fish species. The goal is to use these detailed descriptions to help identify the scientific name of the fish."
 
 elif args.dataset == 'bird-prompting':
-    args.result_dir = root_dir+'VLM4Bio/prompting_results/bird-prompting'
-    images_list_path = root_dir+'VLM4Bio/datasets/Bird/metadata/imagelist_prompting.txt'
-    image_dir = root_dir+'VLM4Bio/datasets/Bird/images'
-    img_metadata_path = root_dir+'VLM4Bio/datasets/Bird/metadata/metadata_prompting.csv'
+    args.result_dir = osp.join(args.result_dir, 'bird-prompting')
+    images_list_path = osp.join(args.data_dir, 'VLM4Bio/datasets/Bird/metadata/imagelist_prompting.txt')
+    image_dir = osp.join(args.data_dir, 'VLM4Bio/datasets/Bird/images')
+    img_metadata_path = osp.join(args.data_dir, 'VLM4Bio/datasets/Bird/metadata/metadata_prompting.csv')
     organism = 'bird'
     dense_caption_instruction = "Generate a detailed dense caption for the image of the bird, focusing on describing its physical characteristics, including the size, shape, and coloration of the feathers, beak, and eyes. Describe any distinctive markings or patterns on the body and head. The goal is to use these detailed descriptions to help identify the scientific name of the bird species."
 
 elif args.dataset == 'butterfly-prompting':
-    args.result_dir = root_dir+'VLM4Bio/prompting_results/butterfly-prompting'
-    images_list_path = root_dir+'VLM4Bio/datasets/Butterfly_10k/metadata/imagelist_prompting.txt'
-    image_dir = root_dir+'VLM4Bio/datasets/Butterfly_10k/images'
-    img_metadata_path = root_dir+'VLM4Bio/datasets/Butterfly_10k/metadata/metadata_prompting.csv'
+    args.result_dir = osp.join(args.result_dir, 'butterfly-prompting')
+    images_list_path = osp.join(args.data_dir, 'VLM4Bio/datasets/Butterfly/metadata/imagelist_prompting.txt')
+    image_dir = osp.join(args.data_dir, 'VLM4Bio/datasets/Butterfly/images')
+    img_metadata_path = osp.join(args.data_dir, 'VLM4Bio/datasets/Butterfly/metadata/metadata_prompting.csv')
     organism = 'butterfly'
     dense_caption_instruction = "Generate a detailed dense caption for the image of the butterfly wings. Describe the size, shape, and color patterns of each wing, including any distinctive markings or symmetrical designs. Dense caption should also contain variations in color gradients and the presence of any spots, stripes, or other textural details. The objective is to use these detailed descriptions to assist in identifying the scientific name of the butterfly specimen."
 
@@ -87,67 +68,53 @@ elif args.dataset == 'butterfly-prompting':
 args.result_dir = os.path.join(args.result_dir, args.technique, 'classification' ,args.task_option)
 
 os.makedirs(args.result_dir, exist_ok=True)
+
 print(args.result_dir)
 
 
-print("Arguments: ", args)
+print("Arguments Provided: ", args)
 
 if args.model == 'gpt-4v':
-    
     from interface.gpt import GPT_4V
     model = GPT_4V(model_name="gpt-4v")
     print(f'{args.model} loaded successfully.')
 
-if args.model == 'gpt-4o':
-    
+if args.model == 'gpt-4o':    
     from interface.gpt import GPT_4o
     model = GPT_4o(model_name="gpt-4o")
     print(f'{args.model} loaded successfully.')
 
 if args.model in ['llava-v1.5-7b', 'llava-v1.5-13b']:
-
     from interface.llava import LLaVA
     model_version = args.model                    
     model = LLaVA(
         model_name = model_version,
-        saved_model_dir = f"/projects/ml4science/maruf/new_llava_models/{model_version}.pt"
+        saved_model_dir = osp.join(args.llava_model_dir, f"{model_version}.pt")
     )
 
-if args.model in ['cogvlm-grounding-generalist', 'cogvlm-chat']:
-
+if args.model in ['cogvlm-chat']:
     from interface.cogvlm import CogVLM
-
     model = CogVLM(model_name=args.model)
 
 if args.model in ['minigpt4-vicuna-7B', 'minigpt4-vicuna-13B']:
-    
     from interface.minigpt4 import MiniGPT4
-
     model = MiniGPT4(model_name=args.model,
         cfg_path=f'minigpt4_eval_configs/eval_{args.model}.yaml',
         model_cfg_name=f'{args.model}.yaml'
     )
 
 if args.model in ['blip-flan-xxl', 'blip-flan-xl']:
-    
     from interface.blip import BLIP
-
     model = BLIP(model_name=args.model)
 
 if args.model in ['instruct-vicuna7b', 'instruct-vicuna13b', 'instruct-flant5xl', 'instruct-flant5xxl']:
-    
     from interface.instruct_blip import Instruct_BLIP
-
     model = Instruct_BLIP(model_name=args.model)
 
-##########################################################################################################################
+
 from vlm_datasets.species_dataset import SpeciesClassificationDataset
 import jsonlines
 import json 
-
-# images_list_path = '/projects/ml4science/maruf/Fish_Data/bg_removed/metadata/sample_images.txt'
-# image_dir = '/projects/ml4science/maruf/Fish_Data/bg_removed/INHS'
-# img_metadata_path = '/projects/ml4science/maruf/Fish_Data/bg_removed/metadata/INHS.csv'
 
 with open(images_list_path, 'r') as file:
     lines = file.readlines()
